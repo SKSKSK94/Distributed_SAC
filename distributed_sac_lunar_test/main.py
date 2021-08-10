9a290d0fc7e19b4b4ce916fa9cb51f490d5e8aad
SKSKSK94 <73100569+SKSKSK94@users.noreply.github.com>
2021-08-11 00:59:35 +0900

    2021-08-11

--- distributed_sac_lunar_test/main.py
+++ distributed_sac_lunar_test/main.py
@@ -1,4 +1,3 @@
-#%%
 from player import Player
 from learner import Learner
 import torch
@@ -9,9 +8,10 @@ import time
 import ray
 import os
 
-max_episode_num = 10000
 num_cpus = 4
 num_gpus = 1
+# is_train = True
+is_train = False
 
 Player = ray.remote(num_cpus=1)(Player)
 Learner = ray.remote(num_cpus=2, num_gpus=0)(Learner)
@@ -20,41 +20,31 @@ ray.init(num_cpus=num_cpus, num_gpus=num_gpus)
 
 GPU_NUM = 0
 device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
-cfg_path = '../cfg/cfg.json'
+cfg_path = 'cfg/LunarLanderContinuous-v2_cfg.json'
 
 env = gym.make('LunarLanderContinuous-v2') 
 
 
-#%%
 
-is_train = True
 networks = []
-for task_idx in range(2):
-    networks.append(Player.remote(env, cfg_path, task_idx))
-
-if is_train:
+if is_train:    
+    for task_idx in range(2):
+        networks.append(Player.remote(env, cfg_path, task_idx))
     networks.append(Learner.remote(cfg_path))
+    print('Learner added')
+else:    
+    for task_idx in range(1):
+        networks.append(
+            Player.remote(
+                env, 
+                cfg_path, 
+                task_idx, 
+                train_mode=False,
+                trained_actor_path='distributed_sac_lunar_test/saved_models/distributed_test/lunar_sac_checkpoint_165000.tar',
+                render_mode=True,
+                write_mode=False
+            )
+        )
 
 ray.get([network.run.remote() for network in networks])
-#%%
 ray.shutdown()
-#%%
-
-
-
-
-# #%%
-
-# is_train = False
-# networks = []
-# for task_idx in range(1):
-#     networks.append(Player.remote(env, cfg_path, task_idx, train_mode=False,
-#     trained_actor_path='/home/seokhun/deep-reinforcement-learning/my/CARE/distributed_sac_lunar_test/saved_models/distributed_test/lunar_sac_checkpoint_165000.tar',
-#     render_mode=True))
-
-# if is_train:
-#     networks.append(Learner.remote(cfg_path))
-
-# ray.get([network.run.remote() for network in networks])
-
-# ray.shutdown()
